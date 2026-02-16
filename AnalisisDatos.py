7bec74c5212536fddd85910fbe8a2da347aaefa0
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2026-02-16 07:52:08 -0600

    commit

--- AnalisisDatos.py
+++ AnalisisDatos.py
@@ -1,3 +1,5 @@
 lista = [[1,2],[3,4],[5,6]]
 lista_float = [[float(num) for num in sublista] for sublista in lista]
+forma_dos = [[float(e1), float(e2)] for e1, e2 in lista]
 print(lista_float)
+print(forma_dos)
